7411c673bde22742d19f4ac5b72c603314131509
Breeann Bolinsky <bolinskybm10@gmail.com>
2021-05-26 17:59:40 -0700

    can successfully get character name from regex scrape

---
+++ __tests__/parser.test.js
@@ -0,0 +1,14 @@
+const request = require('../lib/request.js');
+const parse = require('../lib/parser.js');
+
+describe('parse function', () => {
+  it('can parse', async () => {
+    const document = await request();
+    const element = parse(document);
+    console.log(element);
+    expect(element).toEqual(expect.arrayContaining([
+      { name: '1', photoUrl: 'test.jpg', voicedBy: 'Tucker' }     
+    ]));
+
+  });
+});
